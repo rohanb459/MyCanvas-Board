ac154d86e0f7a5bacc8a76344640223ca852309e
Rohan Bhatia <rohanb459@gmail.com>
2022-06-27 21:13:46 +0530

    drag and drop os sticky notes

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+let toolsCont = document.querySelector(".tools-cont");
 let optionsCont = document.querySelector('.options-cont');
+let pencilToolCont = document.querySelector(".pencil-tool-cont");
+let eraserToolCont = document.querySelector(".eraser-tool-cont");
+let pencil= document.querySelector(".pencil");
+let eraser = document.querySelector(".eraser");
+let sticky = document.querySelector(".sticky")
 let optionsFlag = true;
-
+let pencilFlag = false;
+let eraserFlag = false;
 // true -> show tool else false
 
 optionsCont.addEventListener("click", (e)=>{
@@ -20,6 +27,7 @@ function openTools(){
     let iconElem = optionsCont.children[0];
     iconElem.classList.remove("fa-times");
     iconElem.classList.add("fa-bars");
+    toolsCont.style.display = 'flex';
 }
 
 function closeTools()
@@ -27,4 +35,73 @@ function closeTools()
     let iconElem = optionsCont.children[0];
     iconElem.classList.remove("fa-bars");
     iconElem.classList.add("fa-times");
+    toolsCont.style.display = 'none';
+    pencilToolCont.style.display = "none";
+    eraserToolCont.style.display="none";
+}
+
+pencil.addEventListener("click", (e)=>{
+    pencilFlag = !pencilFlag;
+    if(pencilFlag) pencilToolCont.style.display= "block";
+    else pencilToolCont.style.display="none";
+})
+
+eraser.addEventListener("click", (e)=>{
+    eraserFlag = !eraserFlag;
+    if(eraserFlag) eraserToolCont.style.display= "flex";
+    else eraserToolCont.style.display="none";
+})
+
+sticky.addEventListener("click", (e)=>{
+    let stickycont = document.createElement("div");
+    stickycont.setAttribute("class", "sticky-cont");
+    stickycont.innerHTML = `
+        <div class="header-cont">
+            <div class="minimize"></div>
+            <div class="remove"></div>
+        </div>
+        <div class="note-cont">
+        <textarea></textarea>
+        </div>
+    `
+    document.body.appendChild(stickycont);
+
+    stickycont.onmousedown = function(event){
+        dragAndDrop(stickycont, event);
+    };
+
+    stickycont.ondragstart = function(){
+        return false;
+    }
+})
+
+function dragAndDrop(element, event)
+{
+        let shiftX = event.clientX - element.getBoundingClientRect().left;
+        let shiftY = event.clientY - element.getBoundingClientRect().top;
+      
+        element.style.position = 'absolute';
+        element.style.zIndex = 1000;
+      
+        moveAt(event.pageX, event.pageY);
+      
+        // moves the ball at (pageX, pageY) coordinates
+        // taking initial shifts into account
+        function moveAt(pageX, pageY) {
+          element.style.left = pageX - shiftX + 'px';
+          element.style.top = pageY - shiftY + 'px';
+        }
+      
+        function onMouseMove(event) {
+          moveAt(event.pageX, event.pageY);
+        }
+      
+        // move the ball on mousemove
+        document.addEventListener('mousemove', onMouseMove);
+      
+        // drop the ball, remove unneeded handlers
+        element.onmouseup = function() {
+          document.removeEventListener('mousemove', onMouseMove);
+          element.onmouseup = null;
+        };
 }
