fd4a033fe9dc360bbb6c5e9cb1aabc97338b54dc
Rohan Bhatia <rohanb459@gmail.com>
2022-06-28 16:28:20 +0530

    implemented pencil and eraser funtionalities

--- canvas.js
+++ canvas.js
@@ -2,16 +2,83 @@ let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let pencilColor = document.querySelectorAll(".pencil-color");
+let pencilWidthElem = document.querySelector(".pencil-width");
+let eraserWidthElem = document.querySelector(".eraser-width");
+
+let penColor = "red";
+let eraserColor = "white";
+let penWidth = pencilWidthElem.value;
+let eraserWidth = eraserWidthElem.value;
+
+
+
+let mousedown = false;
 // API
 let tool = canvas.getContext("2d");
 
-tool.strokeStyle= "red";
-tool.lineWidth = "5";
+tool.strokeStyle= penColor;
+tool.lineWidth = penWidth;
 
-tool.beginPath();
-tool.moveTo(10,10);
-tool.lineTo(100, 150);
-tool.stroke();
+// mouse down -> start new path
+// mouse move -> path fill(graphics)
+canvas.addEventListener("mousedown", (e)=>{
+    mousedown = true;
+    tool.beginPath();
+    tool.moveTo(e.clientX, e.clientY);
+})
 
-tool.lineTo(200, 200);
-tool.stroke();
+canvas.addEventListener("mousemove", (e)=>{
+    if(mousedown)
+    drawStroke({
+        x: e.clientX,
+        y: e.clientY
+    })
+})
+
+canvas.addEventListener("mouseup", (e)=>{
+    mousedown = false;
+})
+
+function beginPath(strokeObj)
+{
+    tool.beginPath();
+    tool.moveTo(strokeObj.x, strokeObj.y);
+}
+
+function drawStroke(strokeObj)
+{
+    tool.lineTo(strokeObj.x, strokeObj.y);
+    tool.stroke();
+}
+
+pencilColor.forEach((colorElem)=>{
+    colorElem.addEventListener("click", (e)=>{
+        let color = colorElem.classList[0];
+        penColor = color;
+        tool.strokeStyle=penColor;
+    })
+})
+
+pencilWidthElem.addEventListener("change", (e)=>{
+    penWidth = pencilWidthElem.value;
+    tool.lineWidth = penWidth;
+})
+
+eraserWidthElem.addEventListener("change", (e)=>{
+    eraserWidth = eraserWidthElem.value;
+    tool.lineWidth = eraserWidth;
+})
+
+eraser.addEventListener("click", (e)=>{
+     if(eraserFlag)
+     {
+        tool.strokeStyle = eraserColor;
+        tool.lineWidth = eraserWidth;
+     }
+     else
+     {
+        tool.strokeStyle = penColor;
+        tool.lineWidth = penWidth;
+     }
+})
